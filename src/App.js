916e61a5c4702a96b9d33183bad5150127bf0d34
Stetson Ramey <stetson.ramey@gmail.com>
2018-06-10 11:43:24 -0500

    it all works

--- src/App.js
+++ src/App.js
@@ -7,53 +7,84 @@ import characters from "./characters.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting the initial state of things
   state = {
     characters,
-    clicked: false,
-    count: 0
+    clicked: [],
+    score: 0,
+    highscore: 0
   };
 
-  sortCharacters = characters => {
-    let charactersSorted = this.state.characters.sort( (a,b) => {return 0.5 - Math.random()});
-    characters = charactersSorted;
-    this.setState({ characters });
+  // sort the characters function
+  sortCharacters = () => {
+    this.state.characters.sort( (a,b) => {return 0.5 - Math.random()});
   };
 
-  updateScore = () => {
-    this.setState({ count: this.state.count + 1 });
+  // set the high score
+  setHighScore = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({
+        highscore: this.state.score
+      });
+    }
   };
 
-  handleClicked = id => {
-    // 1. check state of friend that is clicked
-    // 2. if state clicked=false set to true
-    // 3. if state clicked=true, end the game
-   
-    this.state.characters.map(item => {
-      // grab ID of current item
-      let currentId = item.id;
-
-      // compare current item with the clicked item id
-      if (currentId === id) {
-        // set clicked to true
-        this.setState({ clicked: true})
-      }
-    });
+  // reset function to reset things when the game is lost
+  resetStuff = () => {
+    this.setHighScore();
+    this.setState({
+      clicked: [],
+      score: 0
+    })
   };
+
+  // character click event
+  characterClick = event => {
+    // grab the id of the clicked character
+    const currentChar = event.target.id;
+    // check to see if that character has been clicked or not, are they in the state array or not?
+    const isClicked = this.state.clicked.indexOf(currentChar) > -1;
+
+    // if that character is clicked, end the game, reset the score
+    if (isClicked) {
+      this.sortCharacters();
+      this.resetStuff();
+    } else {
+      // we have to update the score, sort the cards
+      this.sortCharacters();
+      this.setState({
+        // Lance showed me this trick with the concat
+        clicked: this.state.clicked.concat(currentChar),
+        // increase the score
+        score: this.state.score + 1
+      },
+      // if user gets to all 12, shuffle the cards and reset the game
+      () => {
+        if (this.state.score === 12) {
+          this.sortCharacters();
+          this.resetStuff();
+        }
+      });
+    }
+  };
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
         <div className='App'>
-          <Nav />
+          <Nav 
+          highscore={this.state.highscore}
+          score={this.state.score}
+          />
           <Jumbotron />
             <div className='container'>
               <div className='row'>
                 {this.state.characters.map(character => (
                   <FriendCard
-                    sortCharacters={this.sortCharacters}
+                    characterClick={this.characterClick}
                     id={character.id}
-                    key={character.key}
+                    key={character.id}
                     name={character.name}
                     image={character.image}
                   />
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card col-sm-6 col-lg-3" onClick={ () => props.sortCharacters(props.id) }>
+  <div className="card col-sm-6 col-lg-3" onClick={props.characterClick}>
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+      <img id={props.name} alt={props.name} src={props.image} />
     </div>
   </div>
 );
 
 export default FriendCard;
+  
